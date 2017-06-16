bf8da389e1912e2b57f121ef8dfb516a99f301c1
lubovefremova <efremova191@mail.ru>
2017-06-16 13:09:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from cgi import parse_qs
-from exp_syst_calc import *
-from exp_syst_dict import *
 html = """Введите значения в интервале от 0 до 1 в формате '0.1'.<br>
 <form method="get">С кредитом?<input name="Answer1"></input><br>
 <form method="get">Премиум карта?<input name="Answer2"></input><br>
@@ -11,6 +9,41 @@ html = """Введите значения в интервале от 0 до 1 в
 <form method="get">На физическое/юридические лицо?<input name="Answer7"></input><br>
 <form method="get">MasterCard?<input name="Answer8"></input><br>
 <button>OK</button></form>"""
+def calc(x):
+    ver_var_Debetovaya = {0:0.1, 1:1, 2:0, 3:0, 6:0, 7:1}
+    ver_var_Classic = {0:0.1, 1:1, 2:0, 3:0, 6:1, 7:1}
+    ver_var_Bonusnaya = {0:0.3, 1:1, 2:0, 3:0, 6:1, 7:1}
+    ver_var_CashBack = {0:0.1, 1:0, 2:1, 3:0, 6:0, 8:1}
+    ver_var_Rubls = {0:0.1, 1:0, 2:1, 3:0, 6:1, 8:1}
+    ver_var_Na_ur_litso = {0:0.1, 1:0, 2:0, 3:1, 6:0, 7:1}
+    ver_var_Korporativnaya = {0:0.1, 1:0, 2:0, 3:1, 6:1, 7:1}
+    
+    for i in range(0,len(x)-1):
+        for j in range(0,len(ver_var_Debetovaya)-1):
+            if i==list(ver_var_Debetovaya.keys())[j]:
+                Debetovaya = x[i]*list(ver_var_Debetovaya.values())[j]
+        for k in range(0,len(ver_var_Classic)-1):
+            if i==list(ver_var_Classic.keys())[k]:
+                Classic = x[i]*list(ver_var_Classic())[k]
+        for l in range(0,len(ver_var_Bonusnaya)-1):
+            if i==list(ver_var_Bonusnaya.keys())[l]:
+                Bonusnaya = x[i]*list(ver_var_Sams_S7.values())[l]
+        for m in range(0,len(ver_var_CashBack)-1):
+            if i==list(ver_var_CashBack.keys())[m]:
+                CashBack = x[i]*list(ver_var_CashBack.values())[m]
+        for n in range(0,len(ver_var_Rubls)-1):
+            if i==list(ver_var_Rubls.keys())[n]:
+                Rubls = x[i]*list(ver_var_Rubls.values())[n]
+        for o in range(0,len(ver_var_Na_ur_litso)-1):
+            if i==list(ver_var_Na_ur_litso.keys())[o]:
+                Na_ur_litso = x[i]*list(ver_var_Na_ur_litso.values())[o]
+        for p in range(0,len(ver_var_Korporativnaya)-1):
+            if i==list(ver_var_Korporativnaya.keys())[p]:
+                Korporativnaya = x[i]*list(ver_var_Korporativnaya.values())[p]
+        
+    y=[str(Debetovaya),str(Classic),str(Bonusnaya),str(CashBack),str(Rubls),str(Na_ur_litso),str(Korporativnaya)]
+    return y
+
 def wsgi_app(environ, start_response): 
     response_headers = [('Content-type', 'text/html; charset=UTF-8')]
     response_body = html
